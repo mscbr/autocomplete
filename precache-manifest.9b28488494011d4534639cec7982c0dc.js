c2c39d49c04db134eb3f6f60dce755541064f424
mscbr <scibor.maciej@gmail.com>
2019-12-15 10:29:36 +0100

    Updates

---
+++ precache-manifest.9b28488494011d4534639cec7982c0dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a158499756619c86a9f46e8676e582c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2f18533c3fb50a20a13",
+    "url": "/static/css/main.693b082d.chunk.css"
+  },
+  {
+    "revision": "833fec6539de3d94d07e",
+    "url": "/static/js/2.70a28041.chunk.js"
+  },
+  {
+    "revision": "d2f18533c3fb50a20a13",
+    "url": "/static/js/main.a5f80d46.chunk.js"
+  },
+  {
+    "revision": "f9be2055b6620304f6f1",
+    "url": "/static/js/runtime-main.3e08690f.js"
+  }
+]);
